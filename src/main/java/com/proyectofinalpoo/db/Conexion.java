deb830e5c57c943b548153c9cddbb44149c65afd
jordy1199 <cajasjordy11@gmail.com>
2026-07-04 00:33:47 -0500

    Configuración de la conexión a la base de datos

--- src/main/java/com/proyectofinalpoo/db/Conexion.java
+++ src/main/java/com/proyectofinalpoo/db/Conexion.java
@@ -5,12 +5,32 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
-    // TODO: conectar a la base de datos en la nube (datos placeholder)
-    private static final String url = "jdbc:postgesql://localhost:5432/smth";
-    private static final String user = "postgres";
-    private static final String pass = "postgres";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(url, user, pass);
+    private static final String URL =
+            "jdbc:postgresql://aws-1-us-east-2.pooler.supabase.com:5432/postgres";
+
+    private static final String USER =
+            "postgres.zlubsakadiepinylvcpg";
+
+    private static final String PASSWORD =
+            "#ProyectoPoo";
+
+        public static Connection getConnection() {
+
+            Connection conexion = null;
+
+            try {
+
+                conexion = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            } catch (SQLException e) {
+
+                System.out.println("Error al conectar con la base de datos.");
+                System.out.println("Motivo: " + e.getMessage());
+
+            }
+
+            return conexion;
+
+        }
     }
-}
